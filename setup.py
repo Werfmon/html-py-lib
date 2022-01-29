47ac7e242e89794e80697000863e31cb6342b564
Werfmon <d.vyroubal.w@gmail.com>
2022-01-29 20:18:15 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='html_tags',
+    version='1.0',
+    description='convert python code to HTML',
+    author='Werfmon',
+    packages=['html_tags'],
+    install_requires=['beautifulsoup4'],
+    license='MIT'
+)
